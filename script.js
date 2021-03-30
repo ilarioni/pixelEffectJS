257ecf47743855b2ba2f2ba99ddd18bbcaa85148
ilarioni <ilia.meshvildishvili@gmail.com>
2021-03-30 14:01:59 +0200

    script
    pixel

--- script.js
+++ script.js
@@ -12,4 +12,14 @@ image1.addEventListener('load', function(){
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
     console.log(scannedImage);
+    const scannedData = scannedImage.data;
+    for (let i = 0; i < scannedData.length; i += 4){
+        const total = scannedData[i] + scannedData[i+1] + scannedData[i+2];
+        const averageColorValue = total/3;
+        scannedData[i] = averageColorValue;
+        scannedData[i + 1] = averageColorValue;
+        scannedData[i + 2] = averageColorValue;
+    }
+    scannedImage.data = scannedData;
+    ctx.putImageData(scannedImage, 0, 0);
 })
